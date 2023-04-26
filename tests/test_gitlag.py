b71290353453f4a184b7e4088b9bbbc01657be29
Radetska, Ievgeniia <ievgeniia.radetska@nih.gov>
2023-04-26 17:24:18 -0400

    unsolved version

--- tests/test_gitlag.py
+++ tests/test_gitlag.py
@@ -7,24 +7,10 @@ def test_award_for_mr_mock_gl():
     Test that award_for_mr function calls appropriate GitLab python api functions.
     Mock gitlab_api and project objects.
     """
-    gl_mock = Mock(name="mymock")
+    # setup the gl_mock
 
-    # don't do this:
-    # project_mock = Mock()
-    # gl_mock.project.get.return_value = project_mock
+    # award_for_mr(gl_mock, "baloon", 12, 5)
+    # See https://docs.python.org/3/library/unittest.mock-examples.html#mocking-chained-calls
 
-    award_for_mr(gl_mock, "baloon", 12, 5)
-    # assert gl_mock functions called - see https://docs.python.org/3/library/unittest.mock-examples.html#mocking-chained-calls
-
-    assert gl_mock.projects.get.called_once
-    assert gl_mock.projects.get.call_args.args == (12, )
-
-    print(gl_mock.projects.get)
-
-    assert gl_mock.projects.get.return_value.mergerequests.get.called_once
-    assert gl_mock.projects.get.return_value.mergerequests.get.call_args.args == (5, )
-
-    assert gl_mock.projects.get.return_value.mergerequests.get.return_value.awardemojis.create.called_once
-    assert gl_mock.projects.get.return_value.mergerequests.get.return_value.awardemojis.create.call_args.args == ({"name": "baloon"}, )
-
+    # Assert correct GitLab API functions were called
     assert False
